932bbf6e3dbefc9a08c7cb3e1d70f3c37415f6a3
Ryan Callahan <RyanCallahan312@gmail.com>
2020-02-15 03:10:24 -0500

    added timing and average calculation

--- reaction-speed-cewit/src/components/App.js
+++ reaction-speed-cewit/src/components/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import TimeTest from "./timeTest";
 
-const styles = {};
+//const styles = {};
 
 export default function App(props) {
+    const [lastTime, setLastTime] = React.useState(0);
+    const [times, setTimes] = React.useState([]);
+    const [average, setAverage] = React.useState(null);
+
+    React.useEffect(() => {
+        if (lastTime !== 0) {
+            times.push(lastTime);
+        } else {
+            if (times.length === 3) {
+            }
+        }
+    }, [lastTime, times]);
+
     return (
         <div>
             <p>Hello reaction time!</p>
-            <TimeTest />
+            <TimeTest setLastTime={setLastTime} />
+            <p>
+                {average
+                    ? `your average time is ${average}ms`
+                    : `previous time is ${lastTime}ms`}
+            </p>
         </div>
     );
 }
